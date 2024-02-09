f63d68a732e2841f668e54d788bc3cfe5faed2df
Harsh Kasyap <harshkasyap@gmail.com>
2024-02-09 13:22:46 +0000

    Update config.js
    update ssl settings

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'dpg-cn30m55jm4es73bh5lrg-a.oregon-postgres.render.com',
+    host: env.DB_HOST || 'dpg-cn30m55jm4es73bh5lrg-a.oregon-postgres.render.com?ssl=true',
     port: env.DB_PORT || '5432',
     user: env.DB_USER || 'db_tu4w_user',
     password: env.DB_PASSWORD || 'Kq2KtC1qkSKj591Bc3u40P5XdrUl6cFG',
