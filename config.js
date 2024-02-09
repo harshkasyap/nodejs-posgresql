80f05c375760b14cb1a922fa3ebdd3ccbe84a417
Harsh Kasyap <harshkasyap@gmail.com>
2024-02-09 13:14:21 +0000

    Update config.js
    update db changes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const env = process.env;
 
 const config = {
   db: { /* do not put password or any sensitive info here, done only for demo */
-    host: env.DB_HOST || 'postgres://db_tu4w_user:Kq2KtC1qkSKj591Bc3u40P5XdrUl6cFG@dpg-cn30m55jm4es73bh5lrg-a.oregon-postgres.render.com/db_tu4w',
+    host: env.DB_HOST || 'dpg-cn30m55jm4es73bh5lrg-a.oregon-postgres.render.com',
     port: env.DB_PORT || '5432',
     user: env.DB_USER || 'db_tu4w_user',
     password: env.DB_PASSWORD || 'Kq2KtC1qkSKj591Bc3u40P5XdrUl6cFG',
